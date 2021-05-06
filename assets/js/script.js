3a4aba352feae17ee4d6de7d1a81c7eace77a49f
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-05-05 21:02:42 -0400

    connected api and started moving data to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,86 @@
 // 2. Need API to pull up weather
 // 3. Save to Local Storage
 
-var searchformEl = document.querySelector('#searchForm');
+$(document).ready(function(){
+ var submitBtn = $(".btn-info");
+ var userInput = $("#search-input");
+ var apiKey = "723971c9098a844f6ba34be5f5895d70"
+ 
+    submitBtn.on("click", function(event){
+        event.preventDefault();
 
-function handleSearchFormSubmit(event){
-    event.preventDefault();
+        var userSearch = $(userInput).val().trim();
+        console.log('userSearch:', userSearch);
 
-    var searchInputVal = document.querySelector('#search-input').value
 
-    if (!searchInputVal){
-        console.error('Please type in a search option');
-        
-        
-        return;
-    }
 
-    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+        if($(userInput).val()){
+            $(userInput).val("")
+        }
+       
+        getWeather(userSearch)
+
+
+    })
+
+function getWeather(userSearch){
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${apiKey}`
+    console.log('url:', url)
+    
+    fetch(url).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log('data:', data)
+                
+                var temp = data.main.temp //drilling down to get info
+                console.log('temp:', temp)
+                $(".tempurature").append(temp) //making the data show up on page
+
+
+
+
+            })
+
+        } else{
+            alert("Please add an actual city, dummy")
+        }
+
+
+    }) .catch(function(error){
+        console.log("its dead jim")
+    })
+
+    
+
+
+
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+
+
+})
+
+
+
+// var searchformEl = document.querySelector('#searchForm');
+
+// function handleSearchFormSubmit(event){
+//     event.preventDefault();
+
+//     var searchInputVal = document.querySelector('#search-input').value
+
+//     if (!searchInputVal){
+//         console.error('Please type in a search option');
+        
+        
+//         return;
+//     }
+
+//     var queryString = `./search-results.html?q=${searchInputVal} + &format= + formatInputVal`;
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 
 
