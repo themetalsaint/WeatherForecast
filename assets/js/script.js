d61a4cafec9b351912cd854571eeeb21bb0be028
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-07-21 21:22:48 -0400

    working on localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ var cityCount = 1;
 //Main Weather Function
 $(document).ready(function(){
     var submitBtn = $(".btn-info");
-    var deleteBtn = $("dlt-btn");
+    var deleteBtn = $("#dlt-btn");
     var userInput = $("#search-input");
     var apiKey = "723971c9098a844f6ba34be5f5895d70"
  
@@ -39,6 +39,7 @@ $(document).ready(function(){
         $(".sky").empty();
         $(".wind").empty();
         $(".tempurature").empty();
+        $("#lastResults").empty();
       
         var userSearch = $(userInput).val().trim();
         console.log('userSearch:', userSearch);
@@ -50,80 +51,54 @@ $(document).ready(function(){
         }
        
         getWeather(userSearch)
+        var newCity = userSearch;
 
-        if (localStorage.getItem("history") != null) {
-            var historyTmp = localStorage.getItem("history");
-            var oldhistoryarray = historyTmp.split("|");
-        
-            $("#lastResults").empty();
-        
-            for (var i = 0; i < oldhistoryarray.length; i++) {
-              $("#lastResults").append("<p>" + oldhistoryarray[i] + "</p>");
-            }
-          }
+        if(localStorage.getItem("history") === null){
+            localStorage.setItem("history", "[]")
+        }
+    var searchHistory = JSON.parse(localStorage.getItem("history"));
 
-          //Storing result in previous History localstorage
-          if (localStorage.getItem("history") !== null && localStorage.getItem("history") !== userSearch) {
-            var historyTmp = localStorage.getItem("history");
-            historyTmp += userSearch;
-            localStorage.setItem("history", historyTmp);
-        
-          } else {
-            var historyTmp = "|";
-            localStorage.setItem("history", historyTmp);
-          }
+        if(!searchHistory.includes(newCity)){
+            searchHistory.push(newCity)
+        }
+        localStorage.setItem("history", JSON.stringify(searchHistory));
 
-          function clearHistory() {
-            var searchedCities = JSON.parse(localStorage.getItem("searchedCities"));
-            for (var i = 0; i < searchedCities.length; i++) {
-                $("city-" + searchedCities[i]).remove();
-            }
-            localStorage.clear("searchedCities");
-        };
+        if(localStorage.getItem("history") !== null){
+            var cityHistory = JSON.parse(localStorage.getItem("history"));
         
-        function storeHistory() {
-            // variables to store storage keys for if statements
-            var userSearch = document.querySelector("#search-bar").value.trim().toUpperCase();
-        
-            if (!userSearch) {
-                return;
-            };
-        
-            var previousSearchCity = JSON.parse(localStorage.getItem("searchedCities")) || [];
-            previousSearchCity.push(userSearch);
-            localStorage.setItem("searchedCities", JSON.stringify(previousSearchCity));
-        
-            // clear search bar after clicking search button
-            document.querySelector("#search-bar").value = "";
-        
-            // call function to remove previously searched weather
-            removePrevious();
-        };
-        
-        function loadHistory() {
-            if (localStorage.getItem("searchedCities")) {
-                var previousSearchCity = JSON.parse(localStorage.getItem("searchedCities"));
-                for (var i = 0; i < previousSearchCity.length; i++) {
-                    createBtn(previousSearchCity[i]);
-                }
-            };
-        
-        
-            for (i = 0; i < document.getElementsByClassName("btn").length; i++) {
-                document.getElementsByClassName("btn")[i].addEventListener('click', function () {
-                    var btnClicked = this.getAttribute("data-city");
-                    weatherRequest(btnClicked);
-                    console.log(btnClicked);
-                    removePrevious();
-                });
-            }
-        };
-        
+        console.log(cityHistory)
+
+        for (let index = 0; index < cityHistory.length; index++) {
+            makeLink(cityHistory[index])
+            
+        }
+    }
+})
+
+    function makeLink(text){
+        var link = $("<li class='link'>");
+        link.text(text)
+        var results = $("#lastResults");
+        results.prepend(link);
+    }
+    $(document).on("click", ".link", function(){
+        getWeather(this.innerHTML)
+    })
+
+    deleteBtn.on("click", function(){
+        $("#lastResults").empty()
+        localStorage.clear()
+        location.reload()
     })
 
 function getWeather(userSearch){
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${apiKey}&units=imperial`
     console.log('url:', url)
+        $(".town").empty();
+        $(".icon").empty();
+        $(".sky").empty();
+        $(".wind").empty();
+        $(".tempurature").empty();
     
     fetch(url).then(function(response){
         if(response.ok){
@@ -134,15 +109,6 @@ function getWeather(userSearch){
                 console.log('temp:', temp)
                 $(".tempurature").append("The temperature is: " + temp + " F") //making the data show up on page
               
-                //trying to figure out how to convert to F
-
-                // function temperature (temp) {
-                //     valNum = parseFloat(temp);
-                //     document.getElementById("temp").innerHTML = ((temp-273.15)*1.8)+32;
-                //   }
-                //   temperature();
-
-                //   $(".tempurature").append(valNum)
 
                 var sky = data.weather[0].description //drilling down to get info
                 console.log('sky:', sky)
@@ -164,26 +130,13 @@ function getWeather(userSearch){
         } else{
             alert("Please add an actual city, dummy")
         }
-        
-        // remove previously searched weather info
-        var removePrevious = function () {
-            cityNameEl.remove();
-            uvIndexContainer.remove();
-            forecastContainer.innerHTML = "";
-            currentTempEl.remove();
-            humidityEl.remove();
-            windEl.remove();
-        };
-
-    }) .catch(function(error){
-        console.log("its dead jim")
+    
     })
 
     
 
 }
-    loadHistory();
-    deleteBtn.addEventListener("click", clearHistory);
+    
 
 
 })
