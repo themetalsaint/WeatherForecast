384d2c4cac68c55be5719ac0b684adc192f81cb9
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-07-25 17:29:55 -0400

    finally finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@ var uvValueDisplay = document.createElement("div");
 
 // 5 day forecast variables
 var forecastContainer = document.querySelector("#forecast-result");
-
 var searchWrapperEl = document.querySelector("#search-wrapper");
 var searchHistoryDiv = document.querySelector("#search-history");
 var cityCount = 1;
@@ -27,6 +26,8 @@ var cityCount = 1;
 
 //Main Weather Function
 $(document).ready(function(){
+
+    $(".toggle").hide();
     var submitBtn = $(".btn-info");
     var deleteBtn = $("#dlt-btn");
     var userInput = $("#search-input");
@@ -40,7 +41,8 @@ $(document).ready(function(){
         $(".wind").empty();
         $(".tempurature").empty();
         $("#lastResults").empty();
-      
+        $(".uv").empty()
+
         var userSearch = $(userInput).val().trim();
         console.log('userSearch:', userSearch);
 
@@ -50,7 +52,8 @@ $(document).ready(function(){
             $(userInput).val("")
         }
        
-        getWeather(userSearch)
+        getWeather(userSearch);
+        fiveDayForecast(userSearch);
         var newCity = userSearch;
 
         if(localStorage.getItem("history") === null){
@@ -99,7 +102,8 @@ function getWeather(userSearch){
         $(".sky").empty();
         $(".wind").empty();
         $(".tempurature").empty();
-    
+        $(".uv").empty();
+
     fetch(url).then(function(response){
         if(response.ok){
             response.json().then(function(data){
@@ -136,8 +140,90 @@ function getWeather(userSearch){
     
 
 }
-    
 
+    //function for 5 Day
+
+ function fiveDayForecast(userSearch){
+     var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&appid=${apiKey}&units=imperial`
+ 
+     fetch(forecastUrl) 
+     .then(function(res){
+         if (res.ok){
+             res.json().then(function(data){
+                 $("#forecast-wrapper").empty();
+                 $(".searchHistory").empty();
+                //  console.log(data);
+                 var fiveDayData = data;
+                 fiveDayData.length = 5;
+                console.log(fiveDayData);
+                $(".searchHistory").text("Search History");
+                $(".toggle").show();
+                for (let i = 0; i < fiveDayData.length; i++) {
+                    var fiveDay = fiveDayData;
+                            // console.log("Five Day Forecast Data: ", fiveDay);
+                            // console.log("Date: ", data.list[i])
+                            var time = i * 8 + 4;
+                            var date = new Date(data.list[time].dt * 1000)
+                            var day = date.getDate();
+                            //How to get long month
+                            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+                            var options = { month: 'long' };
+                            var longMonth = (new Intl.DateTimeFormat('en-US', options).format(date));
+                            var year = date.getFullYear();
+                            var fiveDate = (longMonth + ' ' + day + ', ' + year);
+                            // console.log("City name: ", fiveDay.city.name);
+                            var fiveTemp = ('Temp: ' + fiveDay.list[i].main.temp + ' F');
+                            var fiveDesc = ("Sky: " + fiveDay.list[i].weather[0].description);
+                            var fiveHumidity = ("Humidity: " + fiveDay.list[i].main.humidity + '%');
+                            var fiveWind = ('Wind: ' + fiveDay.list[i].wind.speed + ' MPH');
+                            var thisIcon = (fiveDay.list[i].weather[0].icon);
+                            var fiveIcon = (`https://openweathermap.org/img/wn/${thisIcon}@2x.png`);
+                            var lat = (fiveDay.city.coord.lat);
+                            var lon = (fiveDay.city.coord.lon);
+                            $("#forecast-wrapper").append(`
+                            <div class="card fiveCard">
+                                <div class="card-body">
+                                    <div class="date">${fiveDate}</div>
+                                     <div class="fiveIcon"><img src="${fiveIcon}"></div>
+                                    <div class="temp">${fiveTemp}</div>
+                                    <div class="description">${fiveDesc}</div>
+                                    <div class="fiveHumidity">${fiveHumidity}</div>
+                                    <div class="fiveDayWind">${fiveWind}</div>
+                                </div>
+                            </div>
+                            `)
+                        }
+                        fetchUV(lat, lon)
+                    })
+                }
+     })
+    
+    .catch(function(error){
+        console.log(error);
+    })
+ 
+    }
+        
+
+
+    //function to get UV
+
+    function fetchUV(lat, lon) {
+        var UV = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+        fetch(UV) 
+        .then(function(res){
+            if (res.ok){
+                res.json().then(function(data){
+                    $(".uv").empty()
+                    $(".uv").text("UV index at Noon: "+ data.value + "%" )
+                })
+            }
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
 
 })
 
